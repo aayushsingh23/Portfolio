e544486add2ab6df50cd6016a62775bfe6296d11
Aayush Singh <aayush.singh@gmail.com>
2024-07-05 03:23:41 +0530

    final touch

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
         <Experience/>
         <Tech/>
         <Works/>
-        <div>
+        <div className='relative z-0'>
           <Contact/>
           <StarsCanvas/>
         </div>
