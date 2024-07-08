809f48e1cc3b2a2a9b7d4bde089889c6c49b8e13
Aayush Singh <aayush.singh@gmail.com>
2024-07-08 11:28:02 +0530

    name change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}> Hi, I'm <span className="text-[#915eff]">Demigod</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}> Hi, I'm <span className="text-[#915eff]">Aayush</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop web applications,currently studying <br className="sm:block hidden" /> Data Sciences and Cloud Computing
           </p>
